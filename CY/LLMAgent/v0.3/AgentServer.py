d702f31ed3cd183bc07c70adc4b95b98230b9d2b
ChanyoungPark <chanreverse@gmail.com>
2025-03-26 22:36:41 +0900

    Create AgentServer.py

---
+++ CY/LLMAgent/v0.3/AgentServer.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class IsMaintain(BaseModel):
+    runtime: float
+    failure_rate: float
+    
+@app.post("/IsMaintain")
+def IsMaintain(data: IsMaintain):
+    result = {
+        "need_maintenance": data.failure_rate > 0.15
+    }
+    return {"need_maintenance": result["need_maintenance"]}
